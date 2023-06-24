abde1d98b1eba96e0b6d6dc7d113ad6a2bf7fd6f
Bishnu59273 <bishnusaha59273@gmail.com>
2023-06-24 19:23:36 +0530

    add team section by bishnu

--- header.php
+++ header.php
@@ -148,8 +148,10 @@ if (isset($_POST['Organization'])) {
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
     <link href="assets/plugins/swiper/css/swiper-bundle.min.css" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
     <link href="assets/css/main.css" rel="stylesheet" />
+    <link href="assets/css/avatar.css" rel="stylesheet" />
 
 
     <!--  -->
--- index.php
+++ index.php
@@ -256,4 +256,117 @@
         </center>
     </div>
 </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-8 col-lg-6">
+            <!-- Section Heading-->
+            <div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s" style="
+              visibility: visible;
+              animation-delay: 0.2s;
+              animation-name: fadeInUp;
+            ">
+                <h3 class="teamh">Our Creative <span> Team</span></h3>
+                <p class="teamp">
+                    Appland is completely creative, lightweight, clean &amp; super
+                    responsive app landing page.
+                </p>
+                <div class="line"></div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!-- Single Advisor-->
+        <div class="team">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="
+              visibility: visible;
+              animation-delay: 0.2s;
+              animation-name: fadeInUp;
+            ">
+                <!-- Team Thumb-->
+                <div class="advisor_thumb">
+                    <img src="assets/images/avatar315-removebg-preview.png" alt="" />
+                    <!-- Social Info-->
+                    <div class="social-info">
+                        <a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a
+                            href="#"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div>
+                <!-- Team Details-->
+                <div class="single_advisor_details_info">
+                    <h6>BISHNU SAHA</h6>
+                    <p class="designation">Founder &amp; CEO</p>
+                </div>
+            </div>
+        </div>
+        <!-- Single Advisor-->
+        <div class="team">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.3s" style="
+              visibility: visible;
+              animation-delay: 0.3s;
+              animation-name: fadeInUp;
+            ">
+                <!-- Team Thumb-->
+                <div class="advisor_thumb">
+                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" />
+                    <!-- Social Info-->
+                    <div class="social-info">
+                        <a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a
+                            href="#"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div>
+                <!-- Team Details-->
+                <div class="single_advisor_details_info">
+                    <h6>SHIBAM SAHRMA</h6>
+                    <p class="designation">UI Designer</p>
+                </div>
+            </div>
+        </div>
+        <!-- Single Advisor-->
+        <div class="team">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.4s" style="
+              visibility: visible;
+              animation-delay: 0.4s;
+              animation-name: fadeInUp;
+            ">
+                <!-- Team Thumb-->
+                <div class="advisor_thumb">
+                    <img src="assets/images/avatar315-removebg-preview.png" alt="" />
+                    <!-- Social Info-->
+                    <div class="social-info">
+                        <a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a
+                            href="#"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div>
+                <!-- Team Details-->
+                <div class="single_advisor_details_info">
+                    <h6>SHIBAM SHARMA</h6>
+                    <p class="designation">Developer</p>
+                </div>
+            </div>
+        </div>
+        <!-- Single Advisor-->
+        <div class="team">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.5s" style="
+              visibility: visible;
+              animation-delay: 0.5s;
+              animation-name: fadeInUp;
+            ">
+                <!-- Team Thumb-->
+                <div class="advisor_thumb">
+                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" />
+                    <!-- Social Info-->
+                    <div class="social-info">
+                        <a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a
+                            href="#"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div>
+                <!-- Team Details-->
+                <div class="single_advisor_details_info">
+                    <h6>ROHIT PAUL</h6>
+                    <p class="designation">Marketing Manager</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require "footer.php";?>
